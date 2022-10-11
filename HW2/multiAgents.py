670cfec2c79f9f4faae6d2132733207fd4bd3121
s_du <sungju29@g.skku.edu>
2022-10-11 23:18:00 +0900

    Initial revisement of HW2

--- HW2/multiAgents.py
+++ HW2/multiAgents.py
@@ -74,6 +74,7 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
+
         return childGameState.getScore()
 
 def scoreEvaluationFunction(currentGameState):
@@ -135,6 +136,7 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
+        
         util.raiseNotDefined()
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
